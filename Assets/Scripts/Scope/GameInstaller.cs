a7668a5e1c3cccc2ae42a13b2695dd925f9bd543
Alon Talmi <alon@sparkgames.com>
2026-01-30 10:15:30 +0200

    Oops fix

--- Assets/Scripts/Scope/GameInstaller.cs
+++ Assets/Scripts/Scope/GameInstaller.cs
@@ -7,16 +7,13 @@ namespace Overcrowded
     public class GameInstaller : MonoBehaviour, IInstaller
     {
         [SerializeField] private MaskRegistry _maskRegistry;
-        [SerializeField] private MaskEffectsVisualConfigs _maskEffectsVisualConfigs;
         [SerializeField] private VisualConfigs _visualConfigs;
 
         public void InstallBindings(ContainerBuilder containerBuilder)
         {
             containerBuilder.RegisterValue(_maskRegistry);
             containerBuilder.RegisterValue(new MaskChanger());
-            containerBuilder.RegisterValue(_animationConfigs);
             containerBuilder.RegisterValue(typeof(UserState));
-            containerBuilder.RegisterValue(_maskEffectsVisualConfigs);
             containerBuilder.RegisterValue(_visualConfigs);
         }
     }
