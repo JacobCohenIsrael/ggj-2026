034b6a254810d90a7247f4ecb0029e976f57fb99
Gal Ben Naim <bn.gal@candivore.io>
2026-01-30 21:16:14 +0200

    small fix

--- Assets/Scripts/Scope/LevelInstaller.cs
+++ Assets/Scripts/Scope/LevelInstaller.cs
@@ -11,12 +11,14 @@ namespace Overcrowded
         [SerializeField] private MaskInventory _inventory;
         [SerializeField] private MaskChangerConfigs _changerConfigs;
         [SerializeField] private LevelMenuView _levelMenuView;
+        [SerializeField] private PlayerWonService _playerWonService;
 
         public void InstallBindings(ContainerBuilder containerBuilder)
         {
             containerBuilder.RegisterValue(_inventory);
             containerBuilder.RegisterType(typeof(MaskChanger), Lifetime.Singleton, Resolution.Eager);
             containerBuilder.RegisterType(typeof(PlayerDeathService), Lifetime.Singleton, Resolution.Lazy);
+            containerBuilder.RegisterValue(_playerWonService);
             containerBuilder.RegisterValue(_changerConfigs);
             containerBuilder.RegisterValue(_levelMenuView);
         }
