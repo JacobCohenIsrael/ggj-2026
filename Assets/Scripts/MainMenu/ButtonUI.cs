9428ff114b70ef35bf9cba9c7d8f7f116fe1452e
Jacob Cohen <jacob@pixelpie.games>
2026-01-31 14:21:55 +0200

    Added button ui sounds

--- Assets/Scripts/MainMenu/ButtonUI.cs
+++ Assets/Scripts/MainMenu/ButtonUI.cs
@@ -1,15 +1,18 @@
+using System;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-namespace Overcrowded
+namespace Overcrowded.MainMenu
 {
     [RequireComponent(typeof(Button))]
     public class ButtonUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         [SerializeField] private Vector3 mouseOverTargetScale;
         [SerializeField] private float mouseOverScaleDuration;
+        [SerializeField] private AudioSource fadeAudioSource;
+        [SerializeField] private AudioSource clickAudioSource;
         
         private Vector3 originScale;
         private Button button;
@@ -20,9 +23,27 @@ namespace Overcrowded
             originScale = button.transform.localScale;
         }
 
+        private void OnEnable()
+        {
+            button.onClick.AddListener(OnButtonClicked);
+        }
+
+        private void OnDisable()
+        {
+            button.onClick.RemoveListener(OnButtonClicked);
+        }
+
+        private void OnButtonClicked()
+        {
+            if (clickAudioSource != null)
+                clickAudioSource.Play();
+        }
+
         public void OnPointerEnter(PointerEventData eventData)
         {
             button.transform.DOScale(mouseOverTargetScale, mouseOverScaleDuration).SetUpdate(true);
+            if (fadeAudioSource != null)
+                fadeAudioSource.Play();
         }
 
         public void OnPointerExit(PointerEventData eventData)
