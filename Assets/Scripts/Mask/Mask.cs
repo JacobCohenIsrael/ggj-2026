314610ffd2cc7baf3886fef20af23555b3ded43c
Jacob Cohen <jacob@pixelpie.games>
2026-01-31 10:41:05 +0200

    Project cleanup

--- Assets/Scripts/Mask/Mask.cs
+++ Assets/Scripts/Mask/Mask.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Overcrowded
 {
-    [CreateAssetMenu(menuName = "Create Mask", fileName = "Mask", order = 0)]
+    [CreateAssetMenu(menuName = "Overcrowded/Create Mask", fileName = "Mask", order = 0)]
     public class Mask : ScriptableObject
     {
         [SerializeField] private Sprite _icon;
